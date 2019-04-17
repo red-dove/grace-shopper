889de1e5bcd68878771da3ec6c218391a2a454f5
offthecliff46git <offthecliff46@gmail.com>
2019-04-17 11:13:28 -0400

    last version

--- client/app.js
+++ client/app.js
@@ -1,15 +1,25 @@
 import React from 'react'
-
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
 import {Navbar} from './components'
 import Routes from './routes'
+import store from './store'
+import AllProductsContainer from './components/AllProducts'
 
 const App = () => {
   return (
     <div>
+    <div>
       <Navbar />
       <Routes />
+    </div>
+    <div>
+       <AllProductsContainer  />
+    </div>
     </div>
   )
 }
 
 export default App
+
+
