eb288611d08f543694882e1320923a159d0b3936
Peter Burger <peterburger@gmail.com>
2019-04-22 19:41:52 -0400

    guest cart isLoggedIn fixed

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,74 +1,86 @@
- 
 import React, {Component} from 'react'
-import { connect } from 'react-redux';
-import {Link } from  'react-router-dom'
+import {connect} from 'react-redux'
 import {getSingleProductThunk, addToCartThunk} from '../store'
 
-
 class SingleProduct extends Component {
-    constructor() {
-        super ()
+  constructor(props) {
+    super(props)
 
-        this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  componentDidMount() {
+    this.props.getProduct()
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const product = this.props.data.id
+    const {isLoggedIn} = this.props
+    const guestCart = localStorage.getItem('cart')
+    if (!isLoggedIn && !guestCart) {
+      console.log('not logged in, no guest cart')
+      localStorage.setItem('cart', `[{id: ${product}, quantity: 1}]`)
+    } else if (!isLoggedIn && guestCart) {
+      console.log('not logged in, HAS guest cart')
+      let array = JSON.parse(guestCart)
+      for (let i = 0; i < array.length; i++) {
+        if (array[i].id === product) {
+          array[i].quantity = array[i].quantity + 1
+          break
+        } else {
+          array.push({id: product, quantity: 1})
+        }
       }
-      componentDidMount() { 
-        this.props.getProduct()
-
-     }
-
-     handleSubmit(event) {
-      event.preventDefault();
-      const product = this.props.data.id;
-      this.props.addToCart(product);
- 
+      localStorage.setItem('cart', JSON.stringify(array))
+    } else {
+      console.log('LOGGED IN')
+      this.props.addToCart(product)
     }
-  
-    render() {
+  }
+
+  render() {
     let product = this.props.data
-        return ( 
+    return (
+      <div className="container">
+        <div className="containerSingle">
+          <div>
+            {' '}
+            <img src={product.imageUrl} />{' '}
+          </div>
+          <div id="nameStyle">{product.name}</div>
+          <div id="priceStyle">${product.price}</div>
+          <div>
+            <button type="submit" onClick={this.handleSubmit}>
+              Add to Cart
+            </button>
+          </div>
+        </div>
 
-             <div className='container' >
-                 <div className='containerSingle'>
-                   
-                            <div> <img src={product.imageUrl}  /> </div>  
-                            <div id='nameStyle'>{product.name}</div>
-                              <div id='priceStyle'>${product.price}</div>  
-                              <div><button type="submit" onClick={this.handleSubmit}>Add to Cart</button></div>
-                     </div>
-            
-             <div className='desc'>{product.description}  </div>      
-                    
-                   
-
+        <div className="desc">{product.description} </div>
       </div>
-
-               
-
-        )
-     }   
+    )
+  }
 }
 
-
-const  mapStateToProps= (state)=> {  
+const mapStateToProps = state => {
   //console.log('mapping state to store!!!', state.products.singleProduct)
-      return { data: state.products.singleProduct } 
+  return {
+    data: state.products.singleProduct,
+    isLoggedIn: !!state.user.id
   }
-  
-  const mapDispatchToProps =  (dispatch, id) => {
-    return {
-      getProduct:  ()=> {
-        const productId = id.match.params.productId;
-        dispatch(getSingleProductThunk(productId)) 
-      },
-      addToCart: (product) =>
-      dispatch(addToCartThunk(product)),
+}
 
-    };
-  };
+const mapDispatchToProps = (dispatch, id) => {
+  return {
+    getProduct: () => {
+      const productId = id.match.params.productId
+      dispatch(getSingleProductThunk(productId))
+    },
+    addToCart: product => dispatch(addToCartThunk(product))
+  }
+}
 
-
-
-
-const SingleProductContainer = connect(mapStateToProps, mapDispatchToProps)(SingleProduct);
-export default  SingleProductContainer
-  
+const SingleProductContainer = connect(mapStateToProps, mapDispatchToProps)(
+  SingleProduct
+)
+export default SingleProductContainer
