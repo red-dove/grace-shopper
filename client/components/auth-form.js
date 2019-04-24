c13767f560be3fe37cda4f00fd05a80b509df2b8
Peter Burger <peterburger@gmail.com>
2019-04-24 09:41:47 -0400

    new css

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -10,7 +10,7 @@ const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
 
   return (
-    <div>
+    <div className="container">
       <form onSubmit={handleSubmit} name={name}>
         <div>
           <label htmlFor="email">
@@ -75,7 +75,7 @@ const AuthForm = props => {
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>
-      <a href="/auth/google">{displayName} with Google</a>
+      {/* <a href="/auth/google">{displayName} with Google</a> */}
     </div>
   )
 }
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,29 +6,47 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <nav className='navBar'>
-    <Link to='/profile' ><i className="fas fa-user fa-lg  " ></i> </Link>
-      <Link to="/cart"><i className="fas fa-shopping-cart fa-lg"></i></Link>
-      <div>
-        <Link to="/products">Home</Link>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-left">
+          <div>
+            <Link to="/products">
+              m<i className="fas fa-coffee" />gsy
+            </Link>
+          </div>
+        </div>
+        <div className="navbar-right">
+          <div className="nav-item">
+            <Link to="/profile">
+              <i className="fas fa-user fa-lg  " />{' '}
+            </Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/cart">
+              <i className="fas fa-shopping-cart fa-lg" />
+            </Link>
+          </div>
+          {isLoggedIn ? (
+            <div className="nav-item">
+              {/* The navbar will show these links after you log in */}
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </div>
+          ) : (
+            <>
+              <div className="nav-item">
+                {/* The navbar will show these links before you log in */}
+                <Link to="/login">Login</Link>
+              </div>
+              <div className="nav-item">
+                <Link to="/signup">Sign Up</Link>
+              </div>
+            </>
+          )}
+        </div>
       </div>
-
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
     </nav>
-    <hr />
   </div>
 )
 
